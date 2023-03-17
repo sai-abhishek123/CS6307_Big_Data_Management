568ee6a12e4213c228e4ba01cf1cb5221ba0a414
Thota <sxt210056@utdallas.edu>
2023-03-17 12:16:25 -0500

    Assignment 2

---
+++ Practice_MongoDB/a=0.py
@@ -0,0 +1,16 @@
+# # a=0
+# # while a<5:
+# #     for b in range(1,5):
+# #         if(a+b)%3==0:
+# #             print((a+b)*b,end=' ')
+# #     print()
+# #     a+=2
+
+# def multiply(x,y):
+#     return (x*y)
+# prod=multiply(3,4)
+# print(prod)
+
+a='abc'
+a[1]='b'
+print(a)
